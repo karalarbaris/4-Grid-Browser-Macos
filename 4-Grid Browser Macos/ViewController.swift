40aabaf765569abf47373e16dd02b34277690330
Baris <karalarbaris@gmail.com>
2021-12-27 22:07:18 +0300

    Initial Commit

---
+++ 4-Grid Browser Macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  4-Grid Browser Macos
+//
+//  Created by Baris Karalar on 27.12.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
