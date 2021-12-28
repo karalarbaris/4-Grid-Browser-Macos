38308349dee87d185a10d68e1d78049402229f4c
Baris <karalarbaris@gmail.com>
2021-12-28 16:55:35 +0300

    Layout with stackviews

--- 4-Grid Browser Macos/ViewController.swift
+++ 4-Grid Browser Macos/ViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, WKNavigationDelegate {
 
+    var rows: NSStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 1: Create the stack view and add it to our view
+        rows = NSStackView()
+        rows.orientation = .vertical
+        rows.distribution = .fillEqually
+        rows.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(rows)
+        
+        // 2: Create Auto Layout constraints that pin the stack view to the edges of its container
+        rows.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        rows.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        rows.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        rows.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        // 3: Create an initial column that contains a single web view
+        let column = NSStackView(views: [makeWebView()])
+        
+        // 4: Add this column to the rows stack view
+        rows.addArrangedSubview(column)
 
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
@@ -37,5 +58,13 @@ class ViewController: NSViewController {
         
     }
 
+    func makeWebView() -> NSView {
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        webView.wantsLayer = true
+        webView.load(URLRequest(url: URL(string: "https://www.apple.com/")!))
+        
+        return webView
+    }
 }
 
